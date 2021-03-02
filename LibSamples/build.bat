74005f145b547e8f4d0da5a9f04e5d0162ad23bc
Thiago <thiago.adams@gmail.com>
2021-03-02 16:56:05 -0300

    build

--- LibSamples/build.bat
+++ LibSamples/build.bat
@@ -1,4 +1,5 @@
-cl printenum.c ..\cprime_lib.c
-cl rename.c ..\cprime_lib.c
-cl printtest.c ..\cprime_lib.c
+cl printenum.c ..\cprime_lib.c /Zi /EHsc /Fe
+cl printtest.c ..\cprime_lib.c /Zi /EHsc /Fe
 
+REM To debug: devenv /DebugExe printenum.exe
+REM This avoids a lot of vc++ projects for each test
